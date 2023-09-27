df1a68d32e5bd6c8d90732e7bec5317031739f80
Woo Jia Hao <woojiahao1234@gmail.com>
2023-09-27 20:38:29 +0800

    Add initial Expense model

---
+++ src/main/java/seedu/address/model/expense/Expense.java
@@ -0,0 +1,76 @@
+package seedu.address.model.expense;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+import seedu.address.commons.util.ToStringBuilder;
+
+public class Expense {
+    private final String name;
+    private final double amount;
+    private final String category;
+    private final LocalDate date;
+    private final String location;
+
+    public Expense(String name, double amount, String category, LocalDate date, String location) {
+        this.name = name;
+        this.amount = amount;
+        this.category = category;
+        this.date = date;
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, amount, category, date, location);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Expense)) {
+            return false;
+        }
+
+        Expense otherExpense = (Expense) other;
+        return name.equals(otherExpense.name)
+                && amount == otherExpense.amount
+                && category.equals(otherExpense.category)
+                && date.equals(otherExpense.date)
+                && location.equals(otherExpense.location);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("name", name)
+                .add("amount", amount)
+                .add("category", category)
+                .add("date", date)
+                .add("location", location)
+                .toString();
+    }
+}
