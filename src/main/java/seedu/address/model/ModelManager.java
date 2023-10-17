39f9158f48e297aca8e36bedb300b2ec3ae0257c
Jiahao <woojiahao1234@gmail.com>
2023-10-17 14:08:08 +0800

    Remove addressbook comment
    Co-authored-by: rubesh <rubesh77@gmail.com>

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -67,7 +67,6 @@ public class ModelManager implements Model {
    
 
 
-    //=========== Filtered Person List Accessors =============================================================
 
     @Override
     public Path getUniCashFilePath() {
