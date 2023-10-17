ce3a319146594ec2e19b826ca2b176b026bbcfea
Woo Jia Hao <woojiahao1234@gmail.com>
2023-10-17 08:00:08 +0800

    Add wider test coverage for files

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -19,6 +19,10 @@ public class ParserUtil {
 
     public static final String MESSAGE_INVALID_INDEX = "Index is not a non-zero unsigned integer.";
 
+    private ParserUtil() {
+
+    }
+
     /**
      * Parses {@code oneBasedIndex} into an {@code Index} and returns it. Leading and trailing whitespaces will be
      * trimmed.
--- src/test/java/seedu/address/model/ModelManagerTest.java
+++ src/test/java/seedu/address/model/ModelManagerTest.java
@@ -5,16 +5,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_TRANSACTIONS;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalTransactions.BUYING_GROCERIES;
+import static seedu.address.testutil.TypicalTransactions.INTERN;
 import static seedu.address.testutil.TypicalTransactions.NUS;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.GuiSettings;
+import seedu.address.model.transaction.TransactionNameContainsKeywordsPredicate;
 import seedu.address.model.transaction.exceptions.TransactionNotFoundException;
 import seedu.address.testutil.UniCashBuilder;
 
@@ -71,30 +75,6 @@ public class ModelManagerTest {
         modelManager.setUniCashFilePath(path);
         assertEquals(path, modelManager.getUniCashFilePath());
     }
-
-    // TODO: Add equivalent hasTransaction tests
-
-    //    @Test
-    //    public void hasPerson_nullPerson_throwsNullPointerException() {
-    //        assertThrows(NullPointerException.class, () -> modelManager.hasPerson(null));
-    //    }
-    //
-    //    @Test
-    //    public void hasPerson_personNotInAddressBook_returnsFalse() {
-    //        assertFalse(modelManager.hasPerson(ALICE));
-    //    }
-    //
-    //    @Test
-    //    public void hasPerson_personInAddressBook_returnsTrue() {
-    //        modelManager.addPerson(ALICE);
-    //        assertTrue(modelManager.hasPerson(ALICE));
-    //    }
-    //
-    //    @Test
-    //    public void getFilteredPersonList_modifyList_throwsUnsupportedOperationException() {
-    //        assertThrows(UnsupportedOperationException.class, () -> modelManager
-    //        .getFilteredPersonList().remove(0));
-    //    }
 
     @Test
     public void setUniCash_nullTransaction_throwsNullPointerException() {
@@ -158,12 +138,14 @@ public class ModelManagerTest {
 
         // TODO: Replicate this for transaction list
         // different filteredList -> returns false
-        //String[] keywords = ALICE.getName().fullName.split("\\s+");
-        //modelManager.updateFilteredPersonList(new NameContainsKeywordsPredicate(Arrays.asList(keywords)));
-        //assertFalse(modelManager.equals(new ModelManager(uniCash, userPrefs)));
+        String[] keywords = INTERN.getName().fullName.split("\\s+");
+        modelManager.updateFilteredTransactionList(
+                new TransactionNameContainsKeywordsPredicate(Arrays.asList(keywords))
+        );
+        assertFalse(modelManager.equals(new ModelManager(uniCash, userPrefs)));
 
         // resets modelManager to initial state for upcoming tests
-        //modelManager.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        modelManager.updateFilteredTransactionList(PREDICATE_SHOW_ALL_TRANSACTIONS);
 
         // different userPrefs -> returns false
         UserPrefs differentUserPrefs = new UserPrefs();
