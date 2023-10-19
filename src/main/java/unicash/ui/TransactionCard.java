fedbe224f001b820824d752071ada6b2377a3b61
Rubesh S <rubesh77@gmail.com>
2023-10-19 18:39:17 +0800

    Update Transaction Card

--- src/main/java/unicash/ui/TransactionCard.java
+++ src/main/java/unicash/ui/TransactionCard.java
@@ -1,5 +1,10 @@
 package unicash.ui;
 
+import static unicash.ui.StyleSheet.FONT_STYLE_BOLD;
+import static unicash.ui.StyleSheet.TEXT_FILL_BLACK;
+import static unicash.ui.StyleSheet.TEXT_FILL_GREEN;
+import static unicash.ui.StyleSheet.TEXT_FILL_RED;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
@@ -47,19 +52,48 @@ public class TransactionCard extends UiPart<Region> {
      * All transactions are assumed to be made in dollars.
      */
     public TransactionCard(Transaction transaction, int displayedIndex) {
+        // TODO: Split class into multiple methods for easier styling of each element
         super(FXML);
         this.transaction = transaction;
-        String dollarTransactionWithDecimal = "$" + Amount.amountToDecimalString(transaction.getAmount());
-        String transactionPolarity = transaction.getType().toString().equals("expense") ? "-" : "+";
-
         id.setText(displayedIndex + ". ");
         name.setText(transaction.getName().toString());
-        amount.setText(transactionPolarity + dollarTransactionWithDecimal);
         dateTime.setText(transaction.getDateTime().toString());
         transactionLocation.setText(transaction.getLocation().toString());
-        categories.setText(transaction.getCategories().toString());
+
+        /*
+         * For better presentation of the transaction amounts, instead of using the
+         * toString method of the transaction amount directly, a dollar symbol is prepended
+         * to the transaction, along with the polarity of the transaction depending on whether
+         * it is an income or expense type, Accordingly, text fill styles are also used to
+         * set the color of the amount Label text based on their type.
+         */
+        String dollarTransactionWithDecimal = "$" + Amount.amountToDecimalString(transaction.getAmount());
+        String transactionType = transaction.getType().toString().toLowerCase();
+        String transactionPolarity = transactionType.equals("expense") ? "-" : "+";
+        String transactionCardString = transactionPolarity + dollarTransactionWithDecimal;
+        amount.setText(transactionCardString);
+
+        if (transactionPolarity.equals("-")) { // Set color to red or green depending on amt
+            amount.setStyle(TEXT_FILL_RED);
+        } else if (transactionPolarity.equals("+")) {
+            amount.setStyle(TEXT_FILL_GREEN);
+        } else {
+            amount.setStyle(TEXT_FILL_BLACK);
+        }
+
+        /*
+         * For better presentation of the transaction category, instead of using the
+         * toString method of the UniqueCategoryList directly, the leading and trailing
+         * square brackets are trimmed, and prepended with a "hashtag".
+         */
+        String categoriesToString = transaction.getCategories().toString();
+        int categoriesToStringLength = categoriesToString.length();
+        String trimmedCategoriesToString = categoriesToString
+                .substring(1, categoriesToStringLength - 1);
+        String categoriesToStringWithHashTag = "#" + trimmedCategoriesToString;
+        categories.setText(categoriesToStringWithHashTag);
+        categories.setStyle(FONT_STYLE_BOLD);
 
     }
-
-
 }
+
