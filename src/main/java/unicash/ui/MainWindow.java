5f8b53ea6152188fc84b3f74921f826998af9ac9
Rubesh S <rubesh77@gmail.com>
2023-10-19 21:30:44 +0800

    Add F2 shortcut to help menu and fix style

--- src/main/java/unicash/ui/MainWindow.java
+++ src/main/java/unicash/ui/MainWindow.java
@@ -89,6 +89,7 @@ public class MainWindow extends UiPart<Stage> {
 
     private void setAccelerators() {
         setAccelerator(helpMenuItem, KeyCombination.valueOf("F1"));
+        setAccelerator(userGuideMenuItem, KeyCombination.valueOf("F2"));
     }
 
     /**
