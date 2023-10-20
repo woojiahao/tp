3e68bf1d0a2ab72000218a977851b578a6480539
elhy1999 <ernestliu64@gmail.com>
2023-10-20 15:01:09 +0800

    Add modifications to test find command's ability to find multiple transactions with the same keyword

--- src/test/java/unicash/logic/commands/FindCommandTest.java
+++ src/test/java/unicash/logic/commands/FindCommandTest.java
@@ -5,6 +5,9 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static unicash.logic.UniCashMessages.MESSAGE_TRANSACTIONS_LISTED_OVERVIEW;
 import static unicash.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static unicash.testutil.TypicalTransactions.INTERN;
+import static unicash.testutil.TypicalTransactions.NUS;
+import static unicash.testutil.TypicalTransactions.getTypicalUniCash;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -12,10 +15,12 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import unicash.commons.enums.TransactionType;
 import unicash.model.Model;
 import unicash.model.ModelManager;
 import unicash.model.UniCash;
 import unicash.model.UserPrefs;
+import unicash.model.transaction.Transaction;
 import unicash.model.transaction.TransactionNameContainsKeywordsPredicate;
 
 /**
@@ -23,7 +28,9 @@ import unicash.model.transaction.TransactionNameContainsKeywordsPredicate;
  */
 public class FindCommandTest {
     private final Model model = new ModelManager(new UniCash(), new UserPrefs());
+    private final Model modelWithTransactions = new ModelManager(getTypicalUniCash(), new UserPrefs());
     private final Model expectedModel = new ModelManager(new UniCash(), new UserPrefs());
+
     @Test
     public void equals() {
         TransactionNameContainsKeywordsPredicate firstPredicate =
@@ -47,7 +54,7 @@ public class FindCommandTest {
         // null -> returns false
         assertNotEquals(null, findFirstCommand);
 
-        // different person -> returns false
+        // different transaction -> returns false
         assertNotEquals(findFirstCommand, findSecondCommand);
 
         assertFalse(findFirstCommand.equals(3));
@@ -61,6 +68,22 @@ public class FindCommandTest {
         expectedModel.updateFilteredTransactionList(predicate);
         assertCommandSuccess(command, model, expectedMessage, expectedModel);
         assertEquals(Collections.emptyList(), model.getFilteredTransactionList());
+    }
+
+    @Test
+    public void execute_oneKeyword_multipleTransactionsFound() {
+        String expectedMessage = String.format(MESSAGE_TRANSACTIONS_LISTED_OVERVIEW, 2);
+        TransactionNameContainsKeywordsPredicate predicate = preparePredicate("work");
+        FindCommand command = new FindCommand(predicate);
+
+        Model expectedModel = new ModelManager(getTypicalUniCash(), new UserPrefs());
+        expectedModel.updateFilteredTransactionList(predicate);
+
+        assertCommandSuccess(command, modelWithTransactions, expectedMessage, expectedModel);
+
+        var filteredResult = modelWithTransactions.getFilteredTransactionList();
+        assertEquals(filteredResult.get(0), NUS);
+        assertEquals(filteredResult.get(1), INTERN);
     }
 
     @Test
