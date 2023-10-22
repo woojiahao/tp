ea58655bc5b3d3c6ed325717c35e83062539ec19
Rubesh S <rubesh77@gmail.com>
2023-10-22 14:23:09 +0800

    Add more comments

--- src/main/java/unicash/model/transaction/Transaction.java
+++ src/main/java/unicash/model/transaction/Transaction.java
@@ -66,6 +66,15 @@ public class Transaction {
         return categories;
     }
 
+    /**
+     * Returns the string value associated with this Type
+     *
+     * @return string value of type
+     */
+    public String getTypeString() {
+        return type.type.getOriginalString();
+    }
+
     @Override
     public int hashCode() {
         return Objects.hash(name, amount, dateTime, location, type, categories);
--- src/main/java/unicash/ui/StatusBarFooter.java
+++ src/main/java/unicash/ui/StatusBarFooter.java
@@ -1,5 +1,6 @@
 package unicash.ui;
 
+import static unicash.ui.StyleSheet.TEXT_FILL_BLACK;
 import static unicash.ui.StyleSheet.TEXT_FILL_GREEN;
 import static unicash.ui.StyleSheet.TEXT_FILL_RED;
 
@@ -14,11 +15,14 @@ import javafx.scene.layout.Region;
 import unicash.model.transaction.Transaction;
 
 /**
- * A ui for the status bar that is displayed at the footer of the application.
+ * A UI Controller class for the status bar that is displayed at the
+ * footer of the application. Contains information about data path and
+ * balance indicator.
  */
 public class StatusBarFooter extends UiPart<Region> {
 
     private static final String FXML = "StatusBarFooter.fxml";
+    private static final String BALANCE_FORMAT = "Balance: $%.2f";
 
     @FXML
     private Label saveLocationStatus;
@@ -29,16 +33,28 @@ public class StatusBarFooter extends UiPart<Region> {
     private ObservableList<Transaction> transactions;
 
     /**
-     * Creates a {@code StatusBarFooter} with the given {@code Path}
-     * indicating the current Data Source.
+     * Creates a {@code StatusBarFooter} with the given {@code Path} indicating the
+     * current Data Source and a given {@code ObservableList} of transactions to
+     * facilitate the updating of balance and keeping a balance counter.
      */
     public StatusBarFooter(Path saveLocation,
                            ObservableList<Transaction> transactionList) {
         super(FXML);
         saveLocationStatus.setText("Data source -> " + Paths.get(".").resolve(saveLocation));
         this.transactions = transactionList;
+
+        /*
+         * This initiates the first indication of balance upon application start,
+         * before the method is added as a listener.
+         */
         updateBalance(transactions);
 
+        /*
+         * The {@code updateBalance} method is added as a listener, which means that every
+         * time the transactions list is updated, the balanceIndicator would be updated
+         * dynamically. This is done with a {@code ListChangeListener} that reports a list
+         * of changes done to an {@code ObservableList}
+         */
         this.transactions.addListener((
                 ListChangeListener.Change<? extends Transaction> c) -> {
             updateBalance(transactions);
@@ -46,7 +62,10 @@ public class StatusBarFooter extends UiPart<Region> {
     }
 
     /**
-     * Handles the updating of balanceIndicator in the status bar
+     * Handles the updating of balanceIndicator in the status bar. If the balance
+     * falls below 0, or goes into a negative value, the font style is set to the
+     * universal red color style, and green otherwise.
+     *
      * @param transactions the input transactions list to be monitored.
      */
     @FXML
@@ -54,19 +73,24 @@ public class StatusBarFooter extends UiPart<Region> {
         double balance = 0.0;
 
         for (Transaction t : transactions) {
-            if (t.getType().type.getOriginalString().equalsIgnoreCase("expense")) {
+            if (t.getTypeString().equalsIgnoreCase("expense")) {
                 balance -= t.getAmount().amount;
             } else {
                 balance += t.getAmount().amount;
             }
         }
 
-        balanceIndicator.setText(String.format("Balance: $%.2f", balance));
+        String balanceString = String.format(BALANCE_FORMAT, balance);
+        balanceIndicator.setText(balanceString);
 
-        if (balance < 0) {
+        if (Double.compare(balance, 0) < 0) {
             balanceIndicator.setStyle(TEXT_FILL_RED);
-        } else {
+
+        } else if (Double.compare(balance, 0) > 0) {
             balanceIndicator.setStyle(TEXT_FILL_GREEN);
+
+        } else {
+            balanceIndicator.setStyle(TEXT_FILL_BLACK);
         }
     }
 
