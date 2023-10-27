73aa717c28b6345ae2efbce4f15989fd5a4cf936
Rubesh S <rubesh77@gmail.com>
2023-10-27 20:50:27 +0800

    Improve code style and rename remaining addressbook references

--- src/main/java/unicash/ui/CommandBox.java
+++ src/main/java/unicash/ui/CommandBox.java
@@ -1,5 +1,8 @@
 package unicash.ui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
@@ -9,11 +12,10 @@ import unicash.logic.commands.CommandResult;
 import unicash.logic.commands.exceptions.CommandException;
 import unicash.logic.parser.exceptions.ParseException;
 
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * The UI component that is responsible for receiving user command inputs.
+ * Each user input, regardless of it's correctness
  */
 public class CommandBox extends UiPart<Region> {
 
@@ -25,8 +27,11 @@ public class CommandBox extends UiPart<Region> {
     @FXML
     private TextField commandTextField;
 
-    private List<String> commandHistory = new ArrayList<>();
-    private int currentCommandIndex = -1; // this is to keep track of which command the user is currently on
+    // To keep a history of previous userInputs
+    private List<String> userInputHistory = new ArrayList<>();
+
+    // To keep track of which userInput the user is currently on
+    private int currentUserInputIndex = -1;
 
     /**
      * Creates a {@code CommandBox} with the given {@code CommandExecutor}.
@@ -37,14 +42,23 @@ public class CommandBox extends UiPart<Region> {
         // calls #setStyleToDefault() whenever there is a change to the text of the command box.
         commandTextField.textProperty().addListener((unused1, unused2, unused3) -> setStyleToDefault());
 
-        // Iterates through history of commands held in the commandHistory List
+        /* Iterates through history of userInputs held in the userInputHistory List
+         * the arrow "UP" key and arrow "DOWN" key are constants defined by the KeyCode
+         * class, and user keyboard input is observed by the KeyEvent class. The user can
+         * also input the "ESCAPE" key to blank the field, instead of having to manually
+         * backspace through the Command Text Field */
         commandTextField.setOnKeyPressed(event -> {
             switch (event.getCode()) {
             case UP:
-                showPreviousCommand();
+                showPreviousUserInput();
                 break;
             case DOWN:
-                showNextCommand();
+                showNextUserInput();
+                break;
+            case ESCAPE:
+                clearCommandTextField();
+                break;
+            default:
                 break;
             }
         });
@@ -61,9 +75,9 @@ public class CommandBox extends UiPart<Region> {
         }
 
         try {
-            addCommandToHistory(commandText);
+            addUserInputToHistory(commandText);
             commandExecutor.execute(commandText);
-            commandTextField.setText("");
+            clearCommandTextField();
         } catch (CommandException | ParseException e) {
             setStyleToIndicateCommandFailure();
         }
@@ -102,42 +116,58 @@ public class CommandBox extends UiPart<Region> {
         CommandResult execute(String commandText) throws CommandException, ParseException;
     }
 
-    /* Adds the input string to command history list and resets command index */
-    private void addCommandToHistory(String input) {
-        commandHistory.add(input);
-        currentCommandIndex = -1; // reset index as new command is added
+    /**
+     * Adds the input string to command history list and resets command index.
+     */
+    private void addUserInputToHistory(String input) {
+        userInputHistory.add(input);
+        currentUserInputIndex = -1; // reset index as new command is added
     }
 
-    /* Iterates through command history backwards and sets the commandTextField text */
-    private void showPreviousCommand() {
-        if (commandHistory.isEmpty()) {
+    /**
+     * Clears the Command Text Field.
+     */
+    private void clearCommandTextField() {
+        commandTextField.setText("");
+    }
+
+    /**
+     * Iterates through userInput history backwards and sets the commandTextField text
+     * stopping at the earliest entered user input.
+     */
+    private void showPreviousUserInput() {
+        if (userInputHistory.isEmpty()) {
             return;
         }
 
-        if (currentCommandIndex == -1) {
-            currentCommandIndex = commandHistory.size() - 1;
-        } else if (currentCommandIndex > 0) {
-            currentCommandIndex--;
+        if (currentUserInputIndex == -1) {
+            currentUserInputIndex = userInputHistory.size() - 1;
+
+        } else if (currentUserInputIndex > 0) {
+            currentUserInputIndex--;
         }
-        commandTextField.setText(commandHistory.get(currentCommandIndex));
+
+        commandTextField.setText(userInputHistory.get(currentUserInputIndex));
     }
 
-    /* Iterates through command history forwards and sets the commandTextField text */
-    private void showNextCommand() {
-        if (commandHistory.isEmpty() || currentCommandIndex == -1) {
+    /**
+     * Iterates through userInput history backwards and sets the commandTextField text
+     * stopping at the latest state of the CommandTextField, which is cleared by default.
+     */
+    private void showNextUserInput() {
+        if (userInputHistory.isEmpty() || currentUserInputIndex == -1) {
             return;
         }
 
-        if (currentCommandIndex < commandHistory.size() - 1) {
+        if (currentUserInputIndex < userInputHistory.size() - 1) {
             // If not at the end of the list, move one step forward i.e. down
-            currentCommandIndex++;
-            commandTextField.setText(commandHistory.get(currentCommandIndex));
+            currentUserInputIndex++;
+            commandTextField.setText(userInputHistory.get(currentUserInputIndex));
+
         } else {
-            // If at the end of the list, reset to -1 and clear the input
-            currentCommandIndex = -1;
-            commandTextField.setText("");
+            // If at the end of the list, reset index to -1 and clear text field
+            currentUserInputIndex = -1;
+            clearCommandTextField();
         }
     }
-
-
 }
