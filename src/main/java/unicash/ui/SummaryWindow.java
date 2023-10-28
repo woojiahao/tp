4e73db9f04ef9f00cc10dbb6b7253c9d0e5db700
elhy1999 <ernestliu64@gmail.com>
2023-10-28 15:50:24 +0800

    Update `SummaryWindow` to add line chart and new icon for window
    The summary window now supports only the pie chart. It also uses the
    help window's icon.
    
    Let's integrate the line chart into the summary window, and add a new
    icon for the window instead of using the help window's icon.
    
    The window's icon should be changed because the help window's icon
    contains a '?' symbol, which might confuse users.

--- src/main/java/unicash/ui/SummaryWindow.java
+++ src/main/java/unicash/ui/SummaryWindow.java
@@ -1,13 +1,17 @@
 package unicash.ui;
 
 import java.time.YearMonth;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
 import javafx.scene.chart.PieChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -19,16 +23,23 @@ import unicash.commons.core.LogsCenter;
 public class SummaryWindow extends UiPart<Stage> {
 
     public static final String USER_GUIDE_URL = "https://ay2324s1-cs2103-t16-3.github.io/tp/";
-    public static final String SUMMARY_MESSAGE = "Here is your summary:";
+    public static final String SUMMARY_MESSAGE = "Expense Summary:";
+    public static final String SUMMARY_MESSAGE_WHEN_NO_TRANSACTIONS = "There are no transactions available";
 
     private static final Logger logger = LogsCenter.getLogger(SummaryWindow.class);
     private static final String FXML = "SummaryWindow.fxml";
+    private static final int MAX_DISPLAYED_CATEGORIES = 10;
+    private static final int MAX_DISPLAYED_YEAR_MONTHS = 12;
+    private static final YearMonth EARLIEST_YEAR_MONTH = YearMonth.now().minusMonths(MAX_DISPLAYED_YEAR_MONTHS);
 
     @FXML
     private Label summaryMessage;
 
     @FXML
     private PieChart pieChart;
+
+    @FXML
+    private LineChart<String, Double> lineChart;
 
     @FXML
     private VBox summaryMessageContainer;
@@ -76,22 +87,69 @@ public class SummaryWindow extends UiPart<Stage> {
     }
 
     /**
-     * Updates the pie chart with the data in model.expenseSummary
+     * Updates the pie chart with expense data
+     * Note that only MAX_DISPLAYED_CATEGORIES number of categories will be displayed on the pie chart
      */
     public void setPieChart(HashMap<String, Double> expenseSummary) {
-
-        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
-        for (HashMap.Entry<String, Double> entry : expenseSummary.entrySet()) {
-            String category = entry.getKey();
-            Double totalAmount = entry.getValue();
-            pieChartData.add(new PieChart.Data(category, totalAmount));
-        }
-
         pieChart.getData().clear();
 
-        pieChart.getData().addAll(pieChartData);
-        summaryMessageContainer.layout();
+        if (expenseSummary.isEmpty()) {
+            summaryMessage.setText(SUMMARY_MESSAGE_WHEN_NO_TRANSACTIONS);
+        } else {
+            summaryMessage.setText(SUMMARY_MESSAGE);
+            ObservableList<PieChart.Data> pieChartData = expenseSummary.entrySet().stream()
+                    .map(entry -> new PieChart.Data(entry.getKey(), entry.getValue()))
+                    .sorted(Comparator.comparing(PieChart.Data::getPieValue).reversed())
+                    .limit(MAX_DISPLAYED_CATEGORIES)
+                    .collect(Collectors.toCollection(FXCollections::observableArrayList));
 
+            pieChart.getData().setAll(pieChartData);
+            summaryMessageContainer.layout();
+        }
+    }
+
+    /**
+     * Updates the line chart with expense data
+     * Note that only MAX_DISPLAYED_YEAR_MONTHS number of year-months will be displayed on the line chart
+     */
+    public void setLineGraph(HashMap<YearMonth, Double> expenseSummary) {
+        lineChart.getData().clear();
+
+        fillExpenseSummary(expenseSummary);
+
+        XYChart.Series<String, Double> series = new XYChart.Series<>();
+        ObservableList<XYChart.Data<String, Double>> lineChartData = expenseSummary.entrySet().stream()
+                .sorted(Comparator.comparing(entry -> entry.getKey().toString()))
+                .filter(entry -> isValidYearMonth(entry.getKey()))
+                .map(entry -> new XYChart.Data<>(entry.getKey().toString(), entry.getValue()))
+                .collect(Collectors.toCollection(FXCollections::observableArrayList));
+        series.getData().addAll(lineChartData);
+
+        lineChart.getData().add(series);
+        summaryMessageContainer.layout();
+    }
+
+    /**
+     * Returns true if yearMonth is between now and EARLIEST_YEAR_MONTH, and false otherwise.
+     * Note: This function also returns true if yearMonth is the same as the year-month of now
+     * or EARLIEST_YEAR_MONTH
+     */
+    private static boolean isValidYearMonth(YearMonth yearMonth) {
+        boolean afterMostRecentYearMonth = yearMonth.compareTo(EARLIEST_YEAR_MONTH) >= 0;
+        boolean beforeCurrentYearMonth = yearMonth.compareTo(YearMonth.now()) <= 0;
+        return afterMostRecentYearMonth & beforeCurrentYearMonth;
+    }
+
+    private void fillExpenseSummary(HashMap<YearMonth, Double> expenseSummary) {
+        YearMonth endYearMonth = YearMonth.now();
+
+        YearMonth currentYearMonth = EARLIEST_YEAR_MONTH;
+        while (currentYearMonth.isBefore(endYearMonth)) {
+            if (!expenseSummary.containsKey(currentYearMonth)) {
+                expenseSummary.put(currentYearMonth, 0.0);
+            }
+            currentYearMonth = currentYearMonth.plusMonths(1);
+        }
     }
 
     /**
@@ -115,8 +173,7 @@ public class SummaryWindow extends UiPart<Stage> {
         getRoot().requestFocus();
     }
 
-    public void setLineGraph(HashMap<YearMonth, Double> expenseSummaryPerYearMonth) {
-    }
+
 
 }
 
