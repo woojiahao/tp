8a6a4c85b57b0499f39b6946c18e4d87801b852c
elhy1999 <ernestliu64@gmail.com>
2023-10-28 23:28:56 +0800

    Fix SummaryCommand's test

--- src/test/java/unicash/logic/commands/SummaryCommandTest.java
+++ src/test/java/unicash/logic/commands/SummaryCommandTest.java
@@ -1,7 +1,9 @@
 package unicash.logic.commands;
 
 import static unicash.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static unicash.logic.commands.SummaryCommand.NOT_SHOWING_SUMMARY_MESSAGE;
 import static unicash.logic.commands.SummaryCommand.SHOWING_SUMMARY_MESSAGE;
+import static unicash.testutil.TypicalTransactions.NUS;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,13 +13,20 @@ import unicash.model.ModelManager;
 public class SummaryCommandTest {
     @Test
     public void execute_summary_success() {
-        Model model = new ModelManager();
-        Model expectedModel = new ModelManager();
+        Model emptyModel = new ModelManager();
+        Model expectedEmptyModel = new ModelManager();
+        CommandResult emptyModelExpectedCommandResult =
+                new CommandResult(NOT_SHOWING_SUMMARY_MESSAGE, false, false, false);
+        assertCommandSuccess(new SummaryCommand(), emptyModel,
+                emptyModelExpectedCommandResult, expectedEmptyModel);
 
-        CommandResult expectedCommandResult =
+        Model nonEmptyModel = new ModelManager();
+        nonEmptyModel.addTransaction(NUS);
+        Model expectedNonEmptyModel = new ModelManager();
+        expectedNonEmptyModel.addTransaction(NUS);
+        CommandResult nonEmptyModelExpectedCommandResult =
                 new CommandResult(SHOWING_SUMMARY_MESSAGE, false, false, true);
-
-        assertCommandSuccess(new SummaryCommand(), model,
-                expectedCommandResult, expectedModel);
+        assertCommandSuccess(new SummaryCommand(), nonEmptyModel,
+                nonEmptyModelExpectedCommandResult, expectedNonEmptyModel);
     }
 }
