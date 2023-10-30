5891dbfb8b6552d30cb220a944e78db8f8be27d5
Rubesh S <rubesh77@gmail.com>
2023-10-30 21:49:14 +0800

    Add HelpCommand unit tests

--- src/test/java/unicash/logic/commands/HelpCommandTest.java
+++ src/test/java/unicash/logic/commands/HelpCommandTest.java
@@ -1,12 +1,19 @@
 package unicash.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static unicash.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static unicash.logic.commands.HelpCommand.SHOWING_HELP_MESSAGE;
 
 import org.junit.jupiter.api.Test;
 
+import unicash.commons.util.ToStringBuilder;
 import unicash.model.Model;
 import unicash.model.ModelManager;
+
+
 
 public class HelpCommandTest {
 
@@ -21,4 +28,42 @@ public class HelpCommandTest {
         assertCommandSuccess(new HelpCommand(), model,
                 expectedCommandResult, expectedModel);
     }
+
+    @Test
+    public void multipleHelpCommand_equalsTrue() {
+        HelpCommand helpCommand = new HelpCommand();
+        assertEquals(helpCommand, new HelpCommand());
+
+    }
+
+    @Test
+    public void sameHelpCommand_equalsTrue() {
+        HelpCommand helpCommand = new HelpCommand();
+        assertTrue(helpCommand.equals(helpCommand));
+        assertTrue(helpCommand.equals(new HelpCommand()));
+
+    }
+
+    @Test
+    public void differentCommandTypes_equalsFalse() {
+        Command resetCommand = new ResetCommand();
+        Command helpCommand = new HelpCommand();
+        assertNotEquals(resetCommand, helpCommand);
+        assertFalse(helpCommand.equals(resetCommand));
+        assertFalse(helpCommand.equals(new ResetCommand()));
+    }
+
+    @Test
+    public void nullInput_equalsFalse() {
+        assertNotEquals(null, new HelpCommand());
+        assertFalse(new HelpCommand().equals(null));
+    }
+
+    @Test
+    public void toStringTest() {
+        HelpCommand helpCommand = new HelpCommand();
+        String expected = new ToStringBuilder(new HelpCommand()).toString();
+        assertEquals(expected, helpCommand.toString());
+    }
+
 }
