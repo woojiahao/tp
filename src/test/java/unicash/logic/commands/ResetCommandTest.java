7c60554a9d54a7bbac722d9104f6f769d2e38753
Rubesh S <rubesh77@gmail.com>
2023-10-30 22:01:42 +0800

    Add unit tests for ResetCommand

--- src/test/java/unicash/logic/commands/ResetCommandTest.java
+++ src/test/java/unicash/logic/commands/ResetCommandTest.java
@@ -1,10 +1,15 @@
 package unicash.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static unicash.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static unicash.model.util.SampleDataUtil.getSampleUniCash;
 
 import org.junit.jupiter.api.Test;
 
+import unicash.commons.util.ToStringBuilder;
 import unicash.model.Model;
 import unicash.model.ModelManager;
 import unicash.model.UniCash;
@@ -31,6 +36,54 @@ public class ResetCommandTest {
 
         assertCommandSuccess(new ResetCommand(), model,
                 ResetCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void execute_wrongCommandResult_returnsFalse() {
+        Model model = new ModelManager();
+        Model expectedModel = new ModelManager();
+
+        CommandResult expectedCommandResult =
+                new CommandResult(ResetCommand.MESSAGE_SUCCESS, true, false);
+
+        assertFalse(new ResetCommand().execute(model).equals(expectedCommandResult));
+    }
+
+    @Test
+    public void multipleResetCommand_equalsTrue() {
+        ResetCommand resetCommand = new ResetCommand();
+        assertEquals(resetCommand, new ResetCommand());
+
+    }
+
+    @Test
+    public void sameResetCommand_equalsTrue() {
+        ResetCommand resetCommand = new ResetCommand();
+        assertTrue(resetCommand.equals(resetCommand));
+        assertTrue(resetCommand.equals(new ResetCommand()));
+
+    }
+
+    @Test
+    public void differentCommandTypes_equalsFalse() {
+        Command helpCommand = new HelpCommand();
+        Command resetCommand = new ResetCommand();
+        assertNotEquals(helpCommand, resetCommand);
+        assertFalse(resetCommand.equals(helpCommand));
+        assertFalse(resetCommand.equals(new HelpCommand()));
+    }
+
+    @Test
+    public void nullInput_equalsFalse() {
+        assertNotEquals(null, new ResetCommand());
+        assertFalse(new ResetCommand().equals(null));
+    }
+
+    @Test
+    public void toStringTest() {
+        ResetCommand resetCommand = new ResetCommand();
+        String expected = new ToStringBuilder(new ResetCommand()).toString();
+        assertEquals(expected, resetCommand.toString());
     }
 
 }
